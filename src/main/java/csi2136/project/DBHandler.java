1d58c7182feacda47bd0775210aee9760f2d2da0
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-10 19:49:51 -0500

    Created DBHandler.java  - DBHandler is a Singleton pattern that is responsible for connecting and querying mySQL database
     - DBHandler gets server info from enviormental variables
     - DBHandler creates a connection with DataBase

---
+++ src/main/java/csi2136/project/DBHandler.java
@@ -0,0 +1,60 @@
+package csi2136.project;
+
+import com.mysql.jdbc.Driver;
+import org.hibernate.cfg.NotYetImplementedException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+
+public class DBHandler {
+    private static DBHandler inst;
+
+    private String host;
+    private String port;
+    private String name;
+    private String userName;
+    private String password;
+
+    private DBHandler(){
+        Map<String, String> env = System.getenv();
+
+        host = env.get("DB_host");
+        port = env.get("DB_port");
+        name = env.get("DB_name");
+
+        userName = env.get("DB_username");
+        password = env.get("DB_password");
+
+        System.out.println("Host:"+host);
+        System.out.println("port:"+port);
+        System.out.println("name:"+name);
+        System.out.println("userName:"+userName);
+        System.out.println("password:"+password);
+    }
+
+    public static DBHandler getInst(){
+        if (inst == null) {
+            inst = new DBHandler();
+        }
+
+        return inst;
+    }
+
+    public Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(
+            "jdbc:mysql://"+host+":"+port+"/"+name,
+                userName,
+                password
+        );
+    }
+
+    public ResultSet getQuery(String query) {
+        throw new NotYetImplementedException();
+    }
+}
